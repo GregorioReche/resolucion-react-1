29f8dbcf7e3e77ad20c4dc54616cfd2c7bea0417
Gregorio Reche <grego@MacBook-Pro-de-Gregorio.local>
2020-07-10 17:34:57 -0300

    Se soluciona warning sacando la palabra image del atributo alt de la imagen

--- src/components/LastProductInDB.js
+++ src/components/LastProductInDB.js
@@ -10,7 +10,7 @@ function LastProductInDB(){
                     </div>
                     <div className="card-body">
                         <div className="text-center">
-                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25 + 'rem'}} src={product_dummy} alt="image dummy" />
+                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25 + 'rem'}} src={product_dummy} alt="dummy" />
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
                         <a target="_blank" rel="nofollow" href="/">View product detail</a>
